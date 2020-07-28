5ea43abe2c4434309f7f3c06308a14a09f51f287
jabberwoockey <jabberwoockey@gmail.com>
2020-07-28 22:36:03 +0300

    Update

--- 2nginx1docker/www/index.php
+++ 2nginx1docker/www/index.php
@@ -1,25 +1,5 @@
 <?php
-$CONFIG = array (
-  'trusted_proxies' => 
-      array (
-        0 => '172.0.0.0/8',
-      ),
-);
 
-if (!empty($_SERVER['HTTP_CLIENT_IP']))   
-  {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-  }
-//whether ip is from proxy
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
-  {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  }
-//whether ip is from remote address
-else
-  {
-    $ip = $_SERVER['REMOTE_ADDR'];
-  }
 echo '<p align="center">Client IP is: ' . $ip . '</p>';
 
 phpinfo();
